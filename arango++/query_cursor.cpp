df48781e9d58692fcd829a4d9b6015c48213da66
Björn Samvik <bjorn@samvik.se>
2017-06-12 21:34:20 +0200

    Fixed variable that was not initialized.

--- arango++/query_cursor.cpp
+++ arango++/query_cursor.cpp
@@ -4,66 +4,69 @@
 
 #include "query_cursor_private.h"
 
-namespace arango {
-
+namespace arango
+{
 
 query_cursor::query_cursor(std::shared_ptr<impl> p) : p(p)
 {
-
 }
 
 bool query_cursor::has_more() const
 {
-	return p->result.is_null() || p->result["hasMore"].get<bool>();
+    return p->result.is_null() || p->result["hasMore"].get<bool>();
 }
 
 bool query_cursor::get_more()
 {
-	bool result = false;
+    bool result = false;
 
-	if(has_more()) {
-		result = p->get_more();
-	}
+    if(has_more())
+    {
+        result = p->get_more();
+    }
 
-	return result;
+    return result;
 }
 
 int query_cursor::count() const
 {
-	int c = -1;
-	if(p->result) {
-		c = p->result["count"];
-	}
-	return c;
+    int c = -1;
+    if(p->result)
+    {
+        c = p->result["count"];
+    }
+    return c;
 }
 
 int query_cursor::batch_size() const
 {
-	return result().size();
+    return result().size();
 }
 
 const json &query_cursor::result() const
 {
-	return p->result["result"];
+    return p->result["result"];
 }
 
 const json &query_cursor::first()
 {
-	json *result;
-	if(get_more()) {
-		result = &p->result["result"][0];
-	}
-	return *result;
+    json *result = nullptr;
+    if(get_more())
+    {
+        result = &p->result["result"][0];
+    }
+    return *result;
 }
 
 const_query_iterator query_cursor::begin() const
 {
-	return const_query_iterator(p->create_cursor, p->read_next, p->delete_cursor);
+    return const_query_iterator(p->create_cursor, p->read_next,
+                                p->delete_cursor);
 }
 
 const_query_iterator query_cursor::end() const
 {
-	return const_query_iterator();
+    return const_query_iterator();
 }
 
 } // namespace arango
